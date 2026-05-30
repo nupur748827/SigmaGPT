e04dfdc083187ec6b0c4761c69207ddf22b7bd84
Nupur <nupursingh2021@gmail.com>
2026-05-30 16:12:08 +0530

    fix cors issue

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use("/api",chatRoutes);
 app.use("/api/auth", authRoutes);
