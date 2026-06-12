046ed305b45b97d4d6b88228a5812f38c93438da
Nupur <nupursingh2021@gmail.com>
2026-06-12 14:39:13 +0530

    Fix context provider

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,14 +7,18 @@ import {MyContext} from "./MyContext.jsx";
 import { useState } from 'react';
 import {v1 as uuidv1} from "uuid";
 import Login from "./Login";
+
+
 function App() {
+
   const [prompt, setPrompt] = useState("");
   const [reply, setReply] = useState(null);
   const [currThreadId, setCurrThreadId] = useState(uuidv1());
-  const [prevChats, setPrevChats] = useState([]); //stores all chats of curr threads
+  const [prevChats, setPrevChats] = useState([]);
   const [newChat, setNewChat] = useState(true);
   const [allThreads, setAllThreads] = useState([]);
   const [token, setToken] = useState(localStorage.getItem("token"));
+
   const providerValues = {
     prompt, setPrompt,
     reply, setReply,
@@ -22,31 +26,29 @@ function App() {
     newChat, setNewChat,
     prevChats, setPrevChats,
     allThreads, setAllThreads,
-    token,setToken
-  }; 
-  
-   
+    token, setToken
+  };
+
   return (
-    <div className='app'>
+    <MyContext.Provider value={providerValues}>
+      <div className='app'>
 
-      {
+        {
+          token ?
 
-        token ?
-      
-      <MyContext.Provider value={providerValues}>
-          <Sidebar></Sidebar>
-          <ChatWindow></ChatWindow>
-        </MyContext.Provider>
-        :
+          <>
+            <Sidebar />
+            <ChatWindow />
+          </>
 
-        <Login />
+          :
 
-      }
+          <Login />
+        }
 
-
-    
-    </div>
-  )
-}
+      </div>
+    </MyContext.Provider>
+  );
+}   // <-- THIS WAS MISSING
 
 export default App;
